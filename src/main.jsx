761b1afd7bbbd4cee70a2614bece3222e298c541
github_username <you@domain.com>
2023-04-09 22:36:22 +0600

    header added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+// import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './components/Header';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
+    // <App />
+ 
 )
